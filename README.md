1d7ab46967490285cf041cfdd9517e2538bcd23b
Oivvio Polite <oivvio@polite.se>
2023-11-20 21:59:16 +0100

    Added user config file

--- README.md
+++ README.md
@@ -14,10 +14,35 @@ Invoking `nestmux` again inside the first session will create a nested session w
 
 Invoking `nestmux` again inside the second session will create a second nested session with the prefix key `C-b`.
 
-## Things to fix
+## Configuration
 
-- Add user configuration of prefix keys and arbitrary nesting depth.
+`nestmux` is configured with a json file located at `~/.config/nestmux/nestmux.json`. The default configuration is
+
+```
+{
+	"prefixes": ["C-h", "C-n", "C-b"],
+	"socket_name": "NESTMUX"
+}
+```
+
+`prefixes` is an arbitrarily long ordered list of prefix keys, where the first element will be the prefix key for the first nesting level and so and. The length of this list controls the maximum nesting depth.
+
+`socket_name` is the name of the `tmux` socket. There's rarely a point in setting this to anything other than the default.
+
+## Things to fix
 
 - Replace `attach_session` with a function replaces the current python process with the `tmux` process we attach to, instead of adding the `tmux` process as a child process of the current python process.
 
 - Add a video to the README.
+
+- Nicer error messages.
+
+- Command to show config.
+
+- Command to list current sessions.
+
+- Command to attach to specific session.
+
+- Add FOSS license
+
+- Move to github
--- nestmux/cli.py
+++ nestmux/cli.py
@@ -1,43 +1,69 @@
 import os
-import libtmux
-from typing import cast, List
+import json
+from pathlib import Path
+from typing import cast, List, TypedDict
 
+import libtmux
 from libtmux.server import Server
 from libtmux.session import Session
 
-PREFIXES = ["C-h", "C-n", "C-b"]
-SOCKET_NAME="NESTMUX"
+
+class Config(TypedDict):
+    prefixes: List[str]
+    socket_name: str
+
+def read_config() -> Config:
+    """
+    Read the config from ~/.config/nestmux/nestmux.json 
+    If that file does not exist or is not valid json, fall back to the default config.
+    """
+    
+    # Set the default config
+    result = cast(Config,{"prefixes": ["C-h", "C-n", "C-b"], "socket_name": "NESTMUX"})
+    try:
+        home = Path.home()
+        config_path = home / ".config" / "nestmux" / "nestmux.json"
+
+        if config_path.exists():
+            result = cast(Config, json.loads(config_path.open().read()))
+
+    except json.decoder.JSONDecodeError:
+        print("Config file invalid, falling back to default config")
+
+    return result    
+
 
 def new_session(prefix:str, server: Server) -> Session:
     """ Create a new session and set it's prefix key """
     session = server.new_session()
 
+    session.set_option("prefix", prefix)
     if prefix != "C-b":
-        session.set_option("prefix", prefix)
         session.cmd("bind-key", prefix, "send-prefix")
         session.cmd("unbind", "C-b")
 
     return session
 
-def attach_session(session: Session):
+def attach_session(session: Session, config: Config):
     # this should be  os.execvp
     # breakpoint()
-    os.system(f"tmux -L {SOCKET_NAME} attach-session -t '{session.name}'")
+    socket_name = config["socket_name"]
+    os.system(f"tmux -L {socket_name} attach-session -t '{session.name}'")
 
-def get_nestinglevel(server: Server, prefixes: List[str]=PREFIXES) -> int:
-    """Figure out how deeply nested we are and return the right prefix key to use"""
+def get_next_nestinglevel(server: Server, config: Config) -> int:
+    """Figure out how deeply nested we are and return the next nesting level """
 
     try:
         socket_name, pid, session_id = os.environ["TMUX"].split(",")
         last_part_of_socket_name = socket_name.split("/")[-1]
-        if last_part_of_socket_name != SOCKET_NAME:
+        if last_part_of_socket_name != config["socket_name"]:
             msg=("Do not you nestmux inside tmux session that are not handled by nestmux")
             raise BaseException(msg)
         else:
             session = cast(Session,server.sessions.get(pid=pid, id=f"${session_id}"))
             prefix = cast(str,session.show_option("prefix"))
 
-            index_of_current_prefix = PREFIXES.index(prefix)
+            index_of_current_prefix =  config["prefixes"].index(prefix)
             index_of_next_prefix = index_of_current_prefix + 1
             return index_of_next_prefix
 
@@ -46,31 +72,30 @@ def get_nestinglevel(server: Server, prefixes: List[str]=PREFIXES) -> int:
         # We are not in a TMUX session, so we are at nesting level 0
         return 0
 
-def start_and_attach_new_session():
+def start_and_attach_new_session(config: Config):
 
 
-    server = libtmux.server.Server(socket_name=SOCKET_NAME)
-    nesting_level = get_nestinglevel(server)
+    server = libtmux.server.Server(socket_name=config["socket_name"])
+    nesting_level = get_next_nestinglevel(server, config)
 
     try:
-        prefix = PREFIXES[nesting_level]
+        prefix = config["prefixes"][nesting_level]
     except IndexError:
         raise BaseException("Too deep")
 
     session = new_session(prefix, server)
 
     if nesting_level == 0:
-        attach_session(session)
+        attach_session(session,config)
     else:
         #Unset TMUX environment variable
-        old_tmux = os.environ["TMUX"]
+        tmux_env = os.environ["TMUX"]
         del(os.environ["TMUX"])
 
-        #attach the session
-        attach_session(session)
+        attach_session(session,config)
 
         #Reset TMUX
-        os.environ = old_tmux
+        os.environ = tmux_env
 
     #parts = cmd.split(" ")
     #print(parts[0], parts)
@@ -78,4 +103,5 @@ def start_and_attach_new_session():
     #os.execlp('sh', 'sh', '-c', cmd)
 
 def main():
-    start_and_attach_new_session()
+    config = read_config()
+    start_and_attach_new_session(config)
